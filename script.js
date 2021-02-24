c903c774a42c8989ea78577c6b77d8a6bbfa13de
Glitch (catkin-honeysuckle-track) <none>
2021-02-24 19:56:33 +0000

    🎙🌑 Checkpoint ./style.css:29018421/503 ./script.js:29018421/731

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+// Global Variables
+var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
+var progress = 0;
+var gamePlaying = false;
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+function startGame() {
+  // initialize game variables
+  progress = 0;
+  gamePlaying = true;
+  // swap the Start and Stop buttons
+  document.getElementById('startBtn').classList.add('hidden');
+  document.getElementById("stopBtn").classList.remove("hidden");
+}
+
+function stopGame() {
+  gamePlaying = false;
+  document.getElementById('startBtn').classList.add('hidden');
+  document.getElementById("stopBtn").classList.remove("hidden");
+}
