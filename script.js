561691a7d22b32527a8c4a1c4ff072426f3c761c
Glitch (catkin-honeysuckle-track) <none>
2021-02-24 20:08:39 +0000

    🚿🗃 Checkpoint ./script.js:29018421/926 ./index.html:29018421/264

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
 var progress = 0;
 var gamePlaying = false;
+var tonePlaying = false;
+var volume = 0.5; // b.t. 0.0 and 1.0
 
 function startGame() {
   // initialize game variables
@@ -14,6 +16,43 @@ function startGame() {
 
 function stopGame() {
   gamePlaying = false;
-  document.getElementById('startBtn').classList.add('hidden');
-  document.getElementById("stopBtn").classList.remove("hidden");
+  document.getElementById('startBtn').classList.remove('hidden');
+  document.getElementById("stopBtn").classList.add("hidden");
 }
+
+// Sound Synthesis Functions
+const freqMap = {
+  1: 261.6,
+  2: 329.6,
+  3: 392,
+  4: 466.2
+}
+function playTone(btn,len){ 
+  o.frequency.value = freqMap[btn]
+  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+  tonePlaying = true
+  setTimeout(function(){
+    stopTone()
+  },len)
+}
+function startTone(btn){
+  if(!tonePlaying){
+    o.frequency.value = freqMap[btn]
+    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+    tonePlaying = true
+  }
+}
+function stopTone(){
+    g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
+    tonePlaying = false
+}
+
+//Page Initialization
+// Init Sound Synthesizer
+var context = new AudioContext()
+var o = context.createOscillator()
+var g = context.createGain()
+g.connect(context.destination)
+g.gain.setValueAtTime(0,context.currentTime)
+o.connect(g)
+o.start(0)
